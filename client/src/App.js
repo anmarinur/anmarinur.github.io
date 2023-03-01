09ff033aa134b949c0f966bc4fb1e0db05fe71b0
anmarinur <andersonmarindev@gmail.com>
2023-02-28 21:30:17 -0500

    Restart project

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
-import { NavBar } from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
